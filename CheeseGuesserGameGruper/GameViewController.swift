e923fa893a5b6910d303c03d56c0ab74d7a918d3
Michael Gruper <sarge6411@icloud.com>
2025-03-04 08:05:51 -0600

    initial commit

--- CheeseGuesserGameGruper/GameViewController.swift
+++ CheeseGuesserGameGruper/GameViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+class Cheese{
+    var name = String()
+    var image = UIImage()
+    
+    init(name: String, image: UIImage) {
+        self.name = name
+        self.image = image
+    }
+}
+
+class AppData{
+    static var points = 0
+    static var gravity = 0.0
+    static var hide = true
+}
+
 class GameViewController: UIViewController {
 
+    var cheeses = [Cheese]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        cheeses.append(Cheese(name: "cheddar", image: UIImage(named: "cheddar")!))
+        cheeses.append(Cheese(name: "brie", image: UIImage(named: "brie")!))
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
@@ -32,14 +51,31 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+            return .portrait
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    
+    
+    @IBAction func guessButton1Action(_ sender: UIButton) {
+        //gouda button as of right now
+        AppData.gravity = 100
+        AppData.hide = false
+    }
+    @IBAction func guessButton2Action(_ sender: UIButton) {
+        //brie button as of now
+        AppData.gravity = 100
+        AppData.hide = false
+    }
+    @IBAction func guessButton3Action(_ sender: UIButton) {
+        //correct button for initial test/cheddar button
+        AppData.gravity = -5
+        AppData.points += 1
+    }
+    @IBAction func musicButtonAction(_ sender: UIButton) {
+        
     }
 }
